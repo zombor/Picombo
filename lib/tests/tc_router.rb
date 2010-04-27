bbe2f8ea56b799f07434b38083624c4162c3dbec
Jeremy Bush <contractfrombelow@gmail.com>
2010-04-27 10:24:45 -0500

    Moving router defs to setup method

--- lib/tests/tc_router.rb
+++ lib/tests/tc_router.rb
@@ -7,22 +7,7 @@ class TestRouter < Test::Unit::TestCase
 	def setup
 		$LOAD_PATH.unshift(File.expand_path(Dir.getwd+'../..')+'/')
 		$LOAD_PATH.unshift(File.expand_path(Dir.getwd+'../../../application')+'/')
-	end
 
-	def test_process
-		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/'))
-		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test'))
-		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test/index'))
-		assert_equal({:controller => 'test', :method => 'index', :params => ['param1', 'param2']}, Picombo::Router.process_uri('/test/index/param1/param2'))
-		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test/index?foo=bar'))
-		assert_equal({:controller => 'test', :method => 'index', :params => ['param1']}, Picombo::Router.process_uri('/test/index/param1?foo=bar'))
-
-		assert_equal({:controller => 'lambda', :method => 'index', :params => []}, Picombo::Router.process_uri('/testing/lambdas'))
-		assert_equal({:controller => 'bar', :method => 'index', :params => []}, Picombo::Router.process_uri('/foo'))
-		assert_equal({:controller => 'city', :method => 'index', :params => ['il', 'chicago']}, Picombo::Router.process_uri('/complex/il/chicago'))
-	end
-
-	def test_new
 		Picombo::Router.add('foo', 'bar/index')
 
 		Picombo::Router.add('bar', lambda{ |path|
@@ -36,5 +21,18 @@ class TestRouter < Test::Unit::TestCase
 				{:controller => 'city', :method => 'index', :params => Regexp.last_match[2,3]}
 			end
 		})
+	end
+
+	def test_process
+		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/'))
+		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test'))
+		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test/index'))
+		assert_equal({:controller => 'test', :method => 'index', :params => ['param1', 'param2']}, Picombo::Router.process_uri('/test/index/param1/param2'))
+		assert_equal({:controller => 'test', :method => 'index', :params => []}, Picombo::Router.process_uri('/test/index?foo=bar'))
+		assert_equal({:controller => 'test', :method => 'index', :params => ['param1']}, Picombo::Router.process_uri('/test/index/param1?foo=bar'))
+
+		assert_equal({:controller => 'lambda', :method => 'index', :params => []}, Picombo::Router.process_uri('/testing/lambdas'))
+		assert_equal({:controller => 'bar', :method => 'index', :params => []}, Picombo::Router.process_uri('/foo'))
+		assert_equal({:controller => 'city', :method => 'index', :params => ['il', 'chicago']}, Picombo::Router.process_uri('/complex/il/chicago'))
 	end
 end
