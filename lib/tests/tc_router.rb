e2c3090e7bfb9ccebce9402c88ae8867679972ac
Jeremy Bush <contractfrombelow@gmail.com>
2010-06-16 17:19:32 -0500

    updating unit tests for routing changes, / isnt needed anymore before routes

--- lib/tests/tc_router.rb
+++ lib/tests/tc_router.rb
@@ -8,11 +8,11 @@ class TestRouter < Test::Unit::TestCase
 		$LOAD_PATH.unshift(File.expand_path(Dir.getwd+'../..')+'/')
 		$LOAD_PATH.unshift(File.expand_path(Dir.getwd+'../../../application')+'/')
 
-		Picombo::Router.add('/foo', 'bar/index')
+		Picombo::Router.add('foo', 'bar/index')
 
 		Picombo::Router.add('bar',
 			lambda{ |path|
-				{:controller => 'lambda', :method => 'index', :params => []} if path == '/testing/lambdas'
+				{:controller => 'lambda', :method => 'index', :params => []} if path == 'testing/lambdas'
 			},
 			'lambda/index'
 		)
@@ -60,6 +60,6 @@ class TestRouter < Test::Unit::TestCase
 		)
 
 		# This one doesn't have a reverse route
-		assert_equal(nil, Picombo::Router.generate('/foo'))
+		assert_equal(nil, Picombo::Router.generate('foo'))
 	end
 end
